5d434f5cd8204bc9079538618a90e1ce84604c94
Junx <jamie@junx.dev>
2022-07-03 17:24:30 +0100

    Add project files.

---
+++ Launcher.Designer.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace GTAWLauncher
+{
+    partial class Launcher
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Launcher));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.Title = new System.Windows.Forms.Label();
+            this.btnLaunch = new System.Windows.Forms.Button();
+            this.picLogo = new System.Windows.Forms.PictureBox();
+            this.cbPriority = new System.Windows.Forms.CheckBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(57)))), ((int)(((byte)(57)))));
+            this.panel1.Controls.Add(this.btnClose);
+            this.panel1.Controls.Add(this.Title);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(475, 50);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnClose.BackColor = System.Drawing.Color.Transparent;
+            this.btnClose.FlatAppearance.BorderSize = 0;
+            this.btnClose.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnClose.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.ForeColor = System.Drawing.Color.Silver;
+            this.btnClose.Location = new System.Drawing.Point(433, 12);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(30, 25);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "X";
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // Title
+            // 
+            this.Title.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.Title.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.ForeColor = System.Drawing.Color.White;
+            this.Title.Location = new System.Drawing.Point(0, 0);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(475, 50);
+            this.Title.TabIndex = 0;
+            this.Title.Text = "GTA World: Launcher";
+            this.Title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnLaunch
+            // 
+            this.btnLaunch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(184)))), ((int)(((byte)(6)))));
+            this.btnLaunch.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLaunch.FlatAppearance.BorderSize = 0;
+            this.btnLaunch.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(199)))), ((int)(((byte)(55)))));
+            this.btnLaunch.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(199)))), ((int)(((byte)(55)))));
+            this.btnLaunch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLaunch.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLaunch.ForeColor = System.Drawing.Color.White;
+            this.btnLaunch.Location = new System.Drawing.Point(12, 291);
+            this.btnLaunch.Name = "btnLaunch";
+            this.btnLaunch.Size = new System.Drawing.Size(451, 47);
+            this.btnLaunch.TabIndex = 1;
+            this.btnLaunch.Text = "Launch";
+            this.btnLaunch.UseVisualStyleBackColor = false;
+            this.btnLaunch.Click += new System.EventHandler(this.btnLaunch_Click);
+            // 
+            // picLogo
+            // 
+            this.picLogo.Image = global::GTAWLauncher.Properties.Resources.Logo;
+            this.picLogo.Location = new System.Drawing.Point(0, 53);
+            this.picLogo.Name = "picLogo";
+            this.picLogo.Size = new System.Drawing.Size(475, 144);
+            this.picLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.picLogo.TabIndex = 2;
+            this.picLogo.TabStop = false;
+            // 
+            // cbPriority
+            // 
+            this.cbPriority.AutoSize = true;
+            this.cbPriority.Location = new System.Drawing.Point(12, 203);
+            this.cbPriority.Name = "cbPriority";
+            this.cbPriority.Size = new System.Drawing.Size(164, 17);
+            this.cbPriority.TabIndex = 3;
+            this.cbPriority.Text = "Set RAGEMP as High Priority";
+            this.cbPriority.UseVisualStyleBackColor = true;
+            this.cbPriority.CheckedChanged += new System.EventHandler(this.cbPriority_CheckedChanged);
+            // 
+            // Launcher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(475, 350);
+            this.Controls.Add(this.cbPriority);
+            this.Controls.Add(this.picLogo);
+            this.Controls.Add(this.btnLaunch);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Launcher";
+            this.ShowIcon = false;
+            this.Text = "GTA: World Launcher";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        /*
+Constants in Windows API
+0x84 = WM_NCHITTEST - Mouse Capture Test
+0x1 = HTCLIENT - Application Client Area
+0x2 = HTCAPTION - Application Title Bar
+
+This function intercepts all the commands sent to the application. 
+It checks to see of the message is a mouse click in the application. 
+It passes the action to the base action by default. It reassigns 
+the action to the title bar if it occured in the client area
+to allow the drag and move behavior.
+*/
+
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case 0x84:
+                    base.WndProc(ref m);
+                    if ((int)m.Result == 0x1)
+                        m.Result = (IntPtr)0x2;
+                    return;
+            }
+
+            base.WndProc(ref m);
+        }
+
+        private Panel panel1;
+        private Button btnClose;
+        private Button btnLaunch;
+        public Label Title;
+        private PictureBox picLogo;
+        private CheckBox cbPriority;
+    }
+}
+
---
+++ Launcher.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GTAWLauncher
+{
+    public partial class Launcher : Form
+    {
+        public Launcher()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLaunch_Click(object sender, EventArgs e)
+        {
+            // launch game
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void cbPriority_CheckedChanged(object sender, EventArgs e)
+        {
+            // priority checkbox changed
+        }
+    }
+}
