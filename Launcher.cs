4add8ff3c74f4f7111fbf49835e1633ba3c03b91
Junx <jamie@junx.dev>
2022-07-03 18:59:05 +0100

    added check for non-admin

--- Launcher.cs
+++ Launcher.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,11 +13,16 @@ namespace GTAWLauncher
 {
     public partial class Launcher : Form
     {
+        public static bool IsAdministrator => new WindowsPrincipal(WindowsIdentity.GetCurrent()).IsInRole(WindowsBuiltInRole.Administrator);
         public Launcher()
         {
             InitializeComponent();
+            // it should not be possible to run this without admin but just incase
+            if (!IsAdministrator)
+            {
+                Application.Exit();
+            }
         }
-
         private void btnLaunch_Click(object sender, EventArgs e)
         {
             // launch game
@@ -32,6 +38,8 @@ namespace GTAWLauncher
             // priority checkbox changed
         }
 
+
+        // allow custom form to be dragged
         private bool dragging = false;
         private Point dragCursorPoint;
         private Point dragFormPoint;
